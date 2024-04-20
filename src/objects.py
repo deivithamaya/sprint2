b3dd5bdaa755551aa5593eea94b6ec3807a06a99
Deivith amaya <amayadeivith@gmail.com>
2024-04-19 22:32:53 -0500

    object.py

--- src/objects.py
+++ src/objects.py
@@ -1,8 +1,15 @@
 from sklearn.pipeline import Pipeline
 import os
+from pathlib import Path
 
 def main():
-    pass
+    #print(dir(os))
+    #print(os.getcwd())
+    path = str(Path(__file__).parent.parent) 
+    if not 'objects' in os.listdir(path):
+        print("the folder is not exists")
+        os.mkdir(path + '/objects')
+    #pass
 
 if __name__=='__main__':
     main()
