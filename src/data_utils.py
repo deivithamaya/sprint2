4cfdd980292f70101acd35da9f3aaf3d8d9fdf0b
Deivith amaya <amayadeivith@gmail.com>
2024-04-22 22:02:14 -0500

    implemented data_utils functions

--- src/data_utils.py
+++ src/data_utils.py
@@ -73,7 +73,11 @@ def get_feature_target(
     # Assign to y_train the "TARGET" column
     # Assign to X_test all the columns from app_test except "TARGET"
     # Assign to y_test the "TARGET" column
-
+    
+    X_train = app_train.drop('TARGET', axis=1)
+    y_train = app_train["TARGET"]
+    X_test = app_test.drop('TARGET', axis=1)
+    y_test = app_test['TARGET']
 
     return X_train, y_train, X_test, y_test
 
@@ -100,7 +104,7 @@ def get_train_val_sets(
         y_val : pd.Series
             Validation target
     """
-    X_train, X_val, y_train, y_val = None, None, None, None
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,stratify=y_train, test_size=0.2, random_state=4)
 
     # TODO
     # Use the `sklearn.model_selection.train_test_split` function with
@@ -110,6 +114,6 @@ def get_train_val_sets(
     # Assign a seed so we get reproducible output across multiple function
     # calls (see `random_state` parameter in `train_test_split`).
     # Shuffle the data (see `shuffle` parameter in `train_test_split`).
-
+    
 
     return X_train, X_val, y_train, y_val
