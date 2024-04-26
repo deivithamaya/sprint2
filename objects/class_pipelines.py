b478eb9edd71f1e2920ca58b0a0fe911f983d9ed
Deivith amaya <amayadeivith@gmail.com>
2024-04-26 17:14:48 -0500

    changes

--- objects/class_pipelines.py
+++ objects/class_pipelines.py
@@ -1,7 +1,16 @@
 import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+"""
+    Create your own function to obtain the data
+    transform it
+"""
+def 
+
 
 class newClass(BaseEstimator):
     def __init__(self, df:pd.DataFrame):
         self.df = df
+        
+    def fit(self):
 
-    def fit(self, )
